994c91a6a57630fc65b70cf5845a32a93d60a6dc
Alan Roth <alan.roth@payara.fish>
2020-09-22 12:44:39 +0100

    Attempt to create a handshake to validate the protocol used

--- appserver/tests/payara-samples/samples/rest-management/src/test/java/fish/payara/samples/rest/management/TLSSupportTest.java
+++ appserver/tests/payara-samples/samples/rest-management/src/test/java/fish/payara/samples/rest/management/TLSSupportTest.java
@@ -1,19 +1,25 @@
 package fish.payara.samples.rest.management;
 
 import com.sun.enterprise.util.JDK;
+import fish.payara.samples.ServerOperations;
 import org.junit.Assume;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.net.SocketFactory;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TLSSupportTest extends RestManagementTest {
     private final List<String> SUPPORTED_SSL_PROTOCOLS = Arrays.asList("TLSv1", "TLSv1.1", "TLSv1.2", "TLSv1.3");
@@ -24,6 +30,7 @@ public class TLSSupportTest extends RestManagementTest {
     }
 
     @Test
+    @Ignore
     public void when_socket_ssl_protocols_returned_expect_supported_tls_versions() throws IOException {
         SocketFactory factory = SSLSocketFactory.getDefault();
         SSLSocket socket = (SSLSocket) factory.createSocket("localhost", 8181);
@@ -31,5 +38,28 @@ public class TLSSupportTest extends RestManagementTest {
 
         assertEquals(SUPPORTED_SSL_PROTOCOLS, sslProtocols);
         assertTrue(Arrays.asList(socket.getSupportedProtocols()).contains("TLSv1.3"));
+    }
+
+    @Test
+    public void when_tls13_is_supported_expect_tls13_used_by_default() throws IOException, NoSuchProviderException, NoSuchAlgorithmException, InterruptedException {
+        SocketFactory factory = SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) factory.createSocket("localhost", 8181);
+
+        addSelfSignedCertificate();
+
+        socket.startHandshake();
+        String protocol = socket.getSession().getProtocol();
+        socket.close();
+        assertEquals("TLSv1.3", protocol);
+    }
+
+    private void addSelfSignedCertificate() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        X509Certificate cert = ServerOperations.createSelfSignedCertificate(keyPair);
+
+        assertNotNull("Failed generating SelfSignedCertificate for test", cert);
+        ServerOperations.addCertificateToContainerTrustStore(cert);
     }
 }
